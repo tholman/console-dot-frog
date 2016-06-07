cfb8a7bf0a7c46c4545609221702e65a8a5f0549
Tim <timothy.w.holman@gmail.com>
2016-06-06 21:41:44 -0400

    console.frog

---
+++ console.frog.js
@@ -0,0 +1,47 @@
+/**
+ *  Console.frog. A better way to log!
+ *    - Logs a frog, saying your log... Yep!
+ *
+ *  MIT licensed
+ *  Copyright (C) 2016 Tim Holman, http://tholman.com
+ */
+
+(function dothething() {
+
+  if( !window.console ) {
+    // Well... nothing really... let it be!
+  }
+
+  window.console.frog = function() {
+
+    var i;
+    
+    // This looks like a frog, right?
+    var frog = [[""],
+                ["         _,-.  "],
+                [" ,-. ,--'  o ) "],
+                [" \\(,' '  ,,-' "],
+                [",-.\\-.__,\\\\_"],
+                ["\\(`--'    `\\ "],
+                [""]];
+
+    // Gets args as a string
+    var args = Array.prototype.slice.call(arguments);
+    var stringOfArgs = args.toString().split(',').join(', ');
+
+    // Add the bubble if there is something to log!
+    if( stringOfArgs.length > 0 ) {  
+      frog[1] = frog[1] + "   ---" + ("-".repeat(stringOfArgs.length)) + "-";
+      frog[2] = frog[2] + "-(   " + stringOfArgs + "   )";
+      frog[3] = frog[3] + "    ---" + ("-".repeat(stringOfArgs.length)) + "-";
+    }
+
+    // Log the frog!
+    for( var i = 0; i < frog.length; i++ ) {
+      console.log(frog[i] + "\n");
+    }
+  }
+
+  console.frog("!");
+
+})();
