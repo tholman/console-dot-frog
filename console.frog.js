7df62fe5853d7d256a415d04afb599ba9a4ba074
Tim <timothy.w.holman@gmail.com>
2016-06-23 21:31:57 -0400

    make better with non browsers

--- console.frog.js
+++ console.frog.js
@@ -16,17 +16,33 @@
   window.console.frog = function() {
 
     var i,
-        css = "color: green";
+      css = "color: green";
+
+    // Should be enough to make this fine in node.
+    var inBrowser = (typeof window !== 'undefined');
     
     // This looks like a frog, right?
     // Taken from here - http://chris.com/ascii/index.php?art=animals/frogs
-    var frog = [
-                "%c         _,-.  %c",
-                "%c ,-. ,--'  o ) %c",
-                "%c \\(,' '  ,,-' %c",
-                "%c,-.\\-.__,\\\\_%c",
-                "%c\\(`--'    `\\ %c",
-                ];
+
+    var frog;
+    if( inBrowser ) {
+      frog = ['%c%c',
+        "%c         _,-.  %c",
+        "%c ,-. ,--'  o ) %c",
+        "%c \\(,' '  ,,-' %c",
+        "%c,-.\\-.__,\\\\_%c",
+        "%c\\(`--'    `\\ %c",
+        '%c%c'];
+    } else {
+      frog = ['',
+        "         _,-.  ",
+        " ,-. ,--'  o ) ",
+        " \\(,' '  ,,-' ",
+        ",-.\\-.__,\\\\_",
+        "\\(`--'    `\\ ",
+        ''];
+    }
+      
 
     // Gets args as a string
     var args = Array.prototype.slice.call(arguments);
@@ -34,14 +50,20 @@
 
     // Add the bubble if there is something to log!
     if( stringOfArgs.length > 0 ) {  
-      frog[0] = frog[0] + "   ---" + ("-".repeat(stringOfArgs.length)) + "-";
-      frog[1] = frog[1] + "-(   " + stringOfArgs + "   )";
-      frog[2] = frog[2] + "    ---" + ("-".repeat(stringOfArgs.length)) + "-";
+      frog[1] = frog[1] + "   ---" + ("-".repeat(stringOfArgs.length)) + "-";
+      frog[2] = frog[2] + "-(   " + stringOfArgs + "   )";
+      frog[3] = frog[3] + "    ---" + ("-".repeat(stringOfArgs.length)) + "-";
     }
 
     // Log the frog!
-    for( i = 0; i < frog.length; i++ ) {
-      console.log(frog[i], css, "");
+    if( inBrowser ) {
+      for( i = 0; i < frog.length; i++ ) {
+        console.log(frog[i], css, "");
+      }
+    } else {
+      for( i = 0; i < frog.length; i++ ) {
+        console.log(frog[i]);
+      }  
     }
   }
 })();
